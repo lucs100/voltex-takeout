604920e08c539452ad4b8ad027d6eaf4d48fe4ca
Lucas Di Pietro <59773132+lucs100@users.noreply.github.com>
2025-01-20 20:53:38 -0500

    Write db transform code

---
+++ songdb_transform.py
@@ -0,0 +1,20 @@
+import json
+
+data = {}
+
+with open("music_db.json", 'r', encoding="utf_8_sig") as file:
+    data = json.load(file)
+
+mid_to_title = {}
+title_to_mid = {}
+
+for song in data["mdb"]["music"]:
+    title = song["info"]["title_name"]
+    id = song["@id"]
+    mid_to_title[id] = title
+    title_to_mid[title] = id
+
+with open("mid_to_title.json", 'w', encoding="utf_8_sig") as file:
+    json.dump(mid_to_title, file, indent=2, ensure_ascii=False)
+with open("title_to_mid.json", 'w', encoding="utf_8_sig") as file:
+    json.dump(title_to_mid, file, indent=2, ensure_ascii=False)
