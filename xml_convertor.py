29ec23a9add995db5be1d7f71cf070679be184eb
Lucas Di Pietro <59773132+lucs100@users.noreply.github.com>
2025-01-22 23:57:53 -0500

    Recompute lookup tables

--- xml_convertor.py
+++ xml_convertor.py
@@ -2,8 +2,12 @@ import xmltodict, json
 
 music_db = {}
 
-with open("data/music_db.xml" ,'r', encoding="cp932") as file:
+# i have no idea what's going on with the encodings here...
+# manually patched anything i could spot that didn't encode right
+# usually just symbols like ♥ , emojis, latin accents, etc that shift-jis can't handle
+# definitely missed a few though
+with open("data/edited_music_db.xml" ,'r', encoding="utf_8_sig") as file:
     music_db = xmltodict.parse(file.read())
 
-with open("data/canonical_music_db.json", 'w', encoding="utf_8_sig") as file:
+with open("data/canonical_fixed_music_db.json", 'w', encoding="utf_8_sig") as file:
     json.dump(music_db, file, indent=4, ensure_ascii=False)
