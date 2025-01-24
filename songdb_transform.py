ff44834b1fba9762348ca9b38ae59fbe1b2fd569
Lucas Di Pietro <59773132+lucs100@users.noreply.github.com>
2025-01-23 22:08:04 -0500

    Additional musicdb fixes / manual aliases

--- songdb_transform.py
+++ songdb_transform.py
@@ -5,13 +5,19 @@ data = {}
 # quite a few songs are inconsistently stored - this adds some aliases
 # mostly just to silence nuisance mismatches
 # eg. both "Alice Maestera" and "Alice Maestera feat. nomico" are keyed to the same mID
+# move this to a file eventually, it's getting rather long...
 MANUAL_ALIASES = {
     "ARROW RAIN": "ARROW RAIN feat. ayame",
     "Alice Maestera": "Alice Maestera feat. nomico",
     "トーホータノシ feat. 抹": "トーホータノシ (feat. 抹)",
     "SACRIFICE feat.ayame": "SACRIFICE feat. ayame",
     "Dreaming feat.nomico": "Dreaming feat. nomico",
-    "グレイスちゃんの超～絶!!グラビティ講座w ": "グレイスちゃんの超～絶!!グラビティ講座w"
+    "グレイスちゃんの超～絶!!グラビティ講座w ": "グレイスちゃんの超～絶!!グラビティ講座w",
+    "Help me， ERINNNNNN!!": "Help me, ERINNNNNN!!",
+    "Help me， ERINNNNNN!! -VENUS mix-": "Help me, ERINNNNNN!! -VENUS mix-",
+    "Help me， ERINNNNNN!! - SH Style -": "Help me, ERINNNNNN!! - SH Style -",
+    "Help me， ERINNNNNN!! -Cranky remix-": "Help me, ERINNNNNN!! -Cranky remix-",
+    "Help me， ERINNNNNN!! #幻想郷ホロイズムver.": "Help me, ERINNNNNN!! #幻想郷ホロイズムver."
 }
 
 
