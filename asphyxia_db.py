9a630d45e99a90e4a5523e0452d9b855309326b5
Lucas Di Pietro <59773132+lucs100@users.noreply.github.com>
2025-05-04 17:04:13 -0400

    Merge csv_translator functions into asphyxia_db.py

--- asphyxia_db.py
+++ asphyxia_db.py
@@ -1,9 +1,20 @@
 from string import ascii_letters, digits
 from random import choices
-import json
+import json, csv
 import shutil
 from pathlib import Path
 from datetime import datetime
+import pandas as pd
+from fuzzywuzzy import process
+from fuzzywuzzy import fuzz
+
+CONFIDENT_MODE = 0
+
+MIDS = {}
+with open("data/title_to_mid.json", 'r', encoding="utf_8_sig") as file:
+    MIDS = json.load(file)
+
+all_titles = MIDS.keys() # for fuzzy search
 
 ID_CHARS = ascii_letters + digits
 def newID():
@@ -127,6 +138,100 @@ def getSaveDataPath(fp: str|Path, dbName: str = "sdvx@asphyxia.db") -> Path|None
     else: 
         return None
 
+def getSongID(title) -> int:
+    """
+    Returns the mID of the passed title, or -1 if no match is found.
+    If CONFIDENT_MODE is True, attempts to fuzzy match.
+    """
+    if title in MIDS.keys(): #exact match
+        return int(MIDS[title])  
+    if CONFIDENT_MODE:
+        try:
+            (match, score) = process.extractOne(title, all_titles, 
+                                scorer=fuzz.token_set_ratio, score_cutoff=80)
+            return MIDS[match]
+        except TypeError: #no sufficient match
+            return -1
+
+def append_mIDs(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Adds a column to a SDVX data file (.csv) with the mID of each song.
+    """
+    dfTitles = df.iloc[:, 0]
+    matching_mIDs = dfTitles.map(getSongID)
+    df.insert(1, "mID", matching_mIDs)
+    df.sort_values("mID", inplace=True)
+    return df
+
+def _getUnknownSongs(df: pd.DataFrame) -> list[str]:
+    """
+    Used for debugging.
+    Gets all songs in a DataFrame which were not successfully matched to an mID.
+    """
+    songIsUnknown = df["mID"] == -1
+    return [x[0] for x in df.loc[songIsUnknown, ['title']].values]
+
+def _saveDF(df: pd.DataFrame, target: str|Path) -> None:
+    """
+    Used for debugging.
+    Saves a DataFrame to an e-amuse SDVX data file (.csv). 
+    """
+    target = Path(target)
+    df.to_csv(target, index=False)
+    # quotes in some songs seem to be messing with csv output (for the csv-lint extension)
+    # df.to_csv(target, index=False, quotechar="`")
+
+def loadScores(source: str|Path) -> pd.DataFrame:
+    """
+    Loads an e-amuse SDVX data file (.csv) into a DataFrame.
+    """
+    source = Path(source)
+    return pd.read_csv(source, encoding="utf_8_sig")
+
+def generateKeysFromDF(df: pd.DataFrame, user_id: str) -> list[dict]:
+    """
+    Returns a list of Asphyxia CORE database keys based on play data from a DataFrame.
+
+    Args:
+        df: The DataFrame of play data.
+        user_id: The Asphyxia CORE user ID (_refID) to associate each play key with. 
+    """
+    data = []
+
+    for idx, row in df.iterrows():
+        if row["mID"] is None:
+            print(f"Warning: Couldn't find music ID for {row['title']}. Skipping.")
+            continue
+        insertTime = int(datetime.now().timestamp())
+        key = {
+            "collection": "music",
+            "mid": int(row["mID"]), #Music ID
+            "type": DIFFICULTY[row["難易度"]],
+            "score": row["ハイスコア"], #High Score
+            "exscore": row["EXスコア"], #EX Score
+            "clear": CLEAR_LAMP[row["クリアランク"]], #Clear Rank 
+            "grade": GRADE[row["スコアグレード"]], #Score Grade
+            "buttonRate": 10, #just a 'score' for each hit type out of 10 - not actually used
+            "longRate": 10,
+            "volRate": 10,
+            "__s": "plugins_profile",
+            "__refid": user_id,
+            "_id": newID(),
+            "createdAt": {
+                "$$date": insertTime #First play of the song 
+                # TODO: investigate: does this do anything? will keys fail to load if times are changed?
+                # ie. is this just used by the db schema, or does asphyxia actually look at it to determine when a song was
+                # first played, when its best was updated, etc?
+            },
+            "updatedAt": {
+                "$$date": insertTime #Time of record insert
+            }
+        }
+        # print(f"Added record: {row['title']} [{row['難易度']}] - {key['score']} ({row['スコアグレード']})")
+        data.append(key)
+    
+    return data
+
 # Song schema: 
 # {
 #   "collection": "music", #always "music"
@@ -149,3 +254,60 @@ def getSaveDataPath(fp: str|Path, dbName: str = "sdvx@asphyxia.db") -> Path|None
 #     "$$date": 1745775105070 #Time of record insert
 #   }
 # }
+
+# Old songDB testing code from csv_translator.py:
+
+## visual diagnostics
+# mostly outdated, still works
+
+# with open(fp, newline='', encoding="utf_8_sig") as csvfile:
+#     sdvxreader = csv.DictReader(csvfile) #TODO: convert to pandas then refactor all this, should make mass-translation and csv copying easier
+#     for row in sdvxreader:
+#         title = row['title']
+#         try: 
+#             mid = mids[row['title']]
+#         except KeyError:
+#             fails.append(title)
+#             try: 
+#                 (match, score) = process.extractOne(title, all_titles, scorer=fuzz.token_set_ratio, score_cutoff=80)
+#             except TypeError as e: #no result
+#                 true_fails.append(title)
+#                 missed_results = process.extract(title, all_titles, scorer=fuzz.token_set_ratio)
+#                 miss_results.append( (title, missed_results) )
+#             else: #this is a mess
+#                 # print(title, mids[match])
+#                 match_results.append( (title, match, score) )
+#                 fuzzypasscnt += 1
+#             finally:
+#                 match = None
+#                 score = None
+#         else:
+#             # print(title, mid)
+#             passcnt += 1
+
+# # --- diagnostics ---
+
+# print("\n\n\n")
+# print(f"--- FUZZY MATCH TABLE: ---")
+# for (title, match, score) in sorted(match_results, key=lambda x: len(x[0])):
+#     print(f"[{score}] \t {title}: \t {match}")
+# print("\n\n\n")
+
+# print(f"--- MISSED MATCH TABLE: ---")
+# for (title, match) in sorted(miss_results, key=lambda x: len(x[0])):
+#     print(f"{title}: \t {match}")
+# print("\n")
+
+# # print("\n\n\n")
+# print(f"--- PASSED: {passcnt} + {fuzzypasscnt} ---")
+# # print("\n\n")
+# print(f"--- FAILED: {len(fails)} ---")
+# # print(fails)
+# # print("\n\n")
+# print(f"--- TRULY FAILED: {len(true_fails)} ---")
+# # print(true_fails)
+
+# #sidenote: https://discord.com/channels/667043164434989061/667066735253520401/1239409904796565594
+# # "currently almost all EG songs are set to hidden in music db" what is an eg song? i think this is why it doesn't appear - i followed up
+
+# #sidenote - directly adding to the savegame file seems to be rejecting
